d9fd7ebd1b17151c5256ae9233fcc5137404d2dc
Sefayet_014 <99322070+Sefayet-Alam@users.noreply.github.com>
2022-07-14 18:55:24 +0600

    Committing files

---
+++ CodeForces/GNU C++14/514A | Chewbaсca and Number/159559410.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ghost 0
+#define PI 3.1415926535897932385
+#define INF 1000111222
+#define eps 1e-7
+#define maxN 1011
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+ 
+const ll md = 1000000007;
+void setIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+map<string,string> mp;
+ll pow1(ll n, ll p)
+{
+    if (p == 0)
+        return 1;
+    ll x = pow1(n, p / 2);
+    x = (x * x) % md;
+    if (p % 2 == 0)
+        return x;
+    else
+        return (x * n) % md;
+}
+ 
+ 
+char in[maxN],out[maxN];
+int main()
+{
+    fast;
+    //setIO();
+    ll t;
+    string s;
+    //cin>>t;
+    t=1;
+    while(t--)
+    {
+    string s;
+    cin>>s;
+    ll n=s.size();
+    for(ll i=0;i<n;i++){
+        if(i==0 && s[i]=='9'){continue;}
+        else if(s[i]>='5'){s[i]='9'-s[i]+'0';}
+    }
+    cout<<s<<endl;
+    }
+ 
+    return ghost;
+}
